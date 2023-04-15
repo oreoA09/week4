7e43a61420e3403efccd17297f690ea31bb68824
oreoA09 <oreo.akanni@gmail.com>
2023-04-15 15:01:00 +0000

    project.put

--- controller/project_controller.js
+++ controller/project_controller.js
@@ -16,6 +16,25 @@ project.get('/new', (req, res)=>{
   res.send('New')
 })
 
+// Edit
+project.get('/:indexArray/edit',(req,res)=>{
+  res.render('Edit',{
+    project:Project[req.params.indexArray],
+    index:req.params.indexArray
+  })
+})
+
+// Update
+project.put('/arrayIndex', (req,res)=>{
+  if(req.body.hasDatabase === "on"){
+    req.body.hasDatabase = 'true'
+  }else{
+    req.body.hasDatabase = 'false'
+  }
+  Project[req.params.arrayIndex] = res.body
+  res.redirect(`/project/${req.params.arrayIndex}')
+}`)
+
 // show as json
 // project.get('/:arrayIndex', (req, res)=>{
 //   res.send(Project[req.params.arrayIndex])
---
+++ views/edit.jsx
@@ -0,0 +1,12 @@
+const React = require("react");
+const DefaultLayout = require("./layouts/default");
+
+function Edit(){
+    return(
+        <DefaultLayout>
+            <h2>Edit Item</h2>
+        </DefaultLayout>
+    )
+}
+
+module.exports = Edit;
